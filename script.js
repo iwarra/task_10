d0f414e4d1ff9d0d03c5b949963d84fbb7bc45b1
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-10-17 16:18:55 +0200

    Task 10 - part 1

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
 const button = document.querySelector('#button');
+const result = document.querySelector('#result');
+const nameError = document.querySelector('#nameError');
 
 function getApi() {
 	const input = document.querySelector('#name');
+	if (!validateInput(input, nameError)) return;
 	const name = input.value.trim();
-	let fullUri = 'https://www.swapi.tech/api/people/?name=' + encodeURIComponent(name);
+	const fullUri = 'https://www.swapi.tech/api/people/?name=' + encodeURIComponent(name);
 	fetch(fullUri)
 		.then((res) => {
 			if (res.ok) return res.json();
 			else throw new Error('Error fetching data:' + res.status);
 		})
 		.then((data) => {
-			console.log(data);
-			//display the following properties: height, mass, gender and hair_color
-			//get result and add HTML and the text to it
+			const creature = data.result[0];
+			result.innerText = `${name} has height: ${creature.properties.height}, mass: ${creature.properties.mass}, hair color: ${creature.properties.hair_color} and is ${creature.properties.gender}.`;
+			input.value = '';
 		})
 		.catch((err) => console.log(err));
 }
 
-button.addEventListener('click', getApi);
+button.addEventListener('click', (e) => {
+	e.preventDefault();
+	getApi();
+});
 
-// Response from Postman
-/*
-{
-    "message": "ok",
-    "result": [
-        {
-            "properties": {
-                "height": "228",
-                "mass": "112",
-                "hair_color": "brown",
-                "skin_color": "unknown",
-                "eye_color": "blue",
-                "birth_year": "200BBY",
-                "gender": "male",
-                "created": "2024-10-16T23:58:13.114Z",
-                "edited": "2024-10-16T23:58:13.114Z",
-                "name": "Chewbacca",
-                "homeworld": "https://www.swapi.tech/api/planets/14",
-                "url": "https://www.swapi.tech/api/people/13"
-            },
-            "description": "A person within the Star Wars universe",
-            "_id": "5f63a36eee9fd7000499be4e",
-            "uid": "13",
-            "__v": 0
-        }
-    ]
+function validateInput(inputEl, messageEl, minLength = 2) {
+	const value = inputEl.value.trim();
+	messageEl.innerText = ''; // Clear previous error messages
+
+	if (value.length < minLength) {
+		messageEl.innerText = `Input must be at least ${minLength} characters long.`;
+		return false;
+	}
+
+	return true;
 }
-*/
