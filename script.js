60920f97e2933147e15b2160cbacdd8a775ef284
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-10-18 11:24:41 +0200

    Modules implemented

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-const button = document.querySelector('#button');
-const result = document.querySelector('#result');
-const nameError = document.querySelector('#nameError');
+import drawCard from './card.js';
+import getApi from './starwars.js';
 
-function getApi() {
-	const input = document.querySelector('#name');
-	if (!validateInput(input, nameError)) return;
-	const name = input.value.trim();
-	const fullUri = 'https://www.swapi.tech/api/people/?name=' + encodeURIComponent(name);
-	fetch(fullUri)
-		.then((res) => {
-			if (res.ok) return res.json();
-			else throw new Error('Error fetching data:' + res.status);
-		})
-		.then((data) => {
-			const creature = data.result[0];
-			if (creature == undefined) {
-				result.innerText = 'No creature found with that name. Please try again.';
-			} else {
-				result.innerText = `${name} has height: ${creature.properties.height}, mass: ${creature.properties.mass}, hair color: ${creature.properties.hair_color} and is ${creature.properties.gender}.`;
-			}
-			input.value = '';
-		})
-		.catch((err) => console.log(err));
-}
-
-button.addEventListener('click', (e) => {
+/////Part 1
+const infoButton = document.querySelector('#getInfoBtn');
+infoButton.addEventListener('click', (e) => {
 	e.preventDefault();
 	getApi();
 });
 
-function validateInput(inputEl, messageEl, minLength = 2) {
-	const value = inputEl.value.trim();
-	messageEl.innerText = ''; // Clear previous error messages
-
-	if (value.length < minLength) {
-		messageEl.innerText = `Input must be at least ${minLength} characters long.`;
-		return false;
-	}
-
-	return true;
-}
+///////Part 2
+const cardButton = document.querySelector('#drawBtn');
+cardButton.addEventListener('click', async (e) => {
+	e.preventDefault();
+	await drawCard();
+});
